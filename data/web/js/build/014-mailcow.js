ba93eb247b4281993cc05d6ef631af40ceb49180
Steffen Sachse <steffen.sachse@uni-rostock.de>
2019-02-08 16:02:49 +0100

    Animation der Passwort-Box ueber transform-Property
    Animieren von left im CSS wird von Browsern nicht gut optimiert und
    sollte vermieden werden

--- data/web/js/build/014-mailcow.js
+++ data/web/js/build/014-mailcow.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
   function shake(div,interval=100,distance=10,times=4) {
     $(div).css('position','relative');
     for(var iter=0;iter<(times+1);iter++){
-      $(div).animate({ left: ((iter%2==0 ? distance : distance*-1))}, interval);
+      $(div).animate({ transform: ((iter%2==0 ? `translateX(${distance})` : `translateX(${distance*-1})`))}, interval);
     }
-    $(div).animate({ left: 0},interval);
+    $(div).animate({ transform: 'translateX(0)'},interval);
   }
 
   // form cache
